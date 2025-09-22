a6d62f4f60dbc0e76f0b716d683700737005ab6e
anlumorgan <anlu.morgan@my.uu.edu>
2025-09-22 16:47:50 -0500

    locations/storyline updated

--- TransformersTextRPG/Program.cs
+++ TransformersTextRPG/Program.cs
@@ -11,19 +11,30 @@ const ConsoleKey X = ConsoleKey.X;
 const ConsoleKey C = ConsoleKey.C;
 const ConsoleKey V = ConsoleKey.V;
 const ConsoleKey ESC = ConsoleKey.Escape;
+const ConsoleKey Y = ConsoleKey.Y;
+const ConsoleKey N = ConsoleKey.N;
 
 
 //Create locations
-const byte LithiumFlats = 0;
-const byte Place2 = 1;
-const byte Place3 = 2;
-const byte Place4 = 3;
-const byte AcidWaste = 4;
+const byte LithiumFlats = 18;
+const byte Praxus = 19;
+const byte Nyon = 20;
+const byte Rodion = 3;
+const byte AcidWastes = 4;
 const byte Iacon = 5;
-byte? currentRegion = Place1;
-
-//For Character Selection
-int CharacterNumber;
+const byte HydraxCity = 6;
+const byte Stanix = 7;
+const byte Altihex = 8;
+const byte Tyrest = 9;
+const byte Praxus2 = 10;
+const byte Iacon2 = 11;
+const byte Altihex2 = 12;
+const byte Tyrest2 = 13;
+const byte Stanix3 = 14;
+const byte AcidWastes3 = 15;
+const byte Stanix2 = 16;
+const byte AcidWastes2 = 17;
+byte? currentRegion = LithiumFlats;
 
 //Three main stats that class determines the baseline
 int Speed;
@@ -111,20 +122,72 @@ while (!exit)
             LithiumFlatsLoop();
             CheckDeath();
             break;
-        case Place2:
-            Place2Loop();
+        case Praxus:
+            PraxusLoop();
             CheckDeath();
             break;
-        case Place3:
-            Place3Loop();
+        case Nyon:
+            NyonLoop();
             CheckDeath();
             break;
-        case AcidWaste:
-            AcidWasteLoop();
+        case Rodion:
+            RodionLoop();
+            CheckDeath();
+            break;
+        case AcidWastes:
+            AcidWastesLoop();
             CheckDeath();
             break;
         case Iacon:
             IaconLoop();
+            CheckDeath();
+            break;
+        case HydraxCity:
+            HydraxCityLoop();
+            CheckDeath();
+            break;
+        case Stanix:
+            StanixLoop();
+            CheckDeath();
+            break;
+        case Altihex:
+            AltihexLoop();
+            CheckDeath();
+            break;
+        case Tyrest:
+            TyrestLoop();
+            CheckDeath();
+            break;
+         case Praxus2:
+            Praxus2Loop();
+            CheckDeath();
+            break;
+        case Stanix2:
+            Stanix2Loop();
+            CheckDeath();
+            break;
+        case Altihex2:
+            Altihex2Loop();
+            CheckDeath();
+            break;
+        case Tyrest2:
+            Tyrest2Loop();
+            CheckDeath();
+            break;
+         case Iacon2:
+            Iacon2Loop();
+            CheckDeath();
+            break;
+        case Stanix3:
+            Stanix3Loop();
+            CheckDeath();
+            break;
+        case AcidWastes2:
+            AcidWastes2Loop();
+            CheckDeath();
+            break;
+        case AcidWastes3:
+            AcidWastes3Loop();
             CheckDeath();
             break;
         default:
@@ -137,36 +200,353 @@ Thread.Sleep(3000);
 
 void CheckDeath()
 {
-    if (HP <= 0)
+    if (Health <= 0)
     {
         currentRegion = null;
+        return;
     }
 }
+
+//Location loops
+//Starting location
 void LithiumFlatsLoop()
 {
+    WriteLine("You have arrived at the Lithium Flats.");
+    WriteLine("Upon arrival you receive a letter, it is from Megatron, he is requesting you to aid him in his fight against the autobots and is planning on launching an attack soon."); //idk abt the letter thing just however the user is informed
+    WriteLine("Megatron leads the Decepticons, a widely disliked group of transformors who wishes to take over the universe and kill the Autobots.");
+    WriteLine("Would you like to help the Autobots and inform them of the threat (Z), side with the Autobots and fight Megatron solo (X), or join the Decepticons (C)? ");
+    switch (GetChoice())
+    {
+        case Z:
+            currentRegion = Praxus;
+            break;
+        case X:
+            currentRegion = Praxus2;
+            break;
+        case C:
+            currentRegion = Nyon;
+            break;
 
+    }
 }
 
-void Place2Loop()
-{ 
-    
-}
-void Place3Loop()
-{ 
-    
-}
-void Place4Loop()
-{ 
-    
-}
-void AcidWasteLoop()
-{ 
-    
-}
+//Destination progression if chose z
+void PraxusLoop()
+{
+    WriteLine("You have arrived at Praxus.");
+    WriteLine(""); // I'm guessing some type of fight will happen
 
+
+    WriteLine("Are you ready to continue your journey to fight the Decepticons? (Y)es or (N)o? ");
+    switch (GetChoice())
+    {
+        case Y:
+            WriteLine("Great!");
+            currentRegion = Iacon;
+            break;
+        case N:
+            WriteLine("That's too bad, maybe we'll see you another time!");
+            currentRegion = LithiumFlats;
+            break;
+
+    }
+}
 void IaconLoop()
-{ 
-    
+{
+    WriteLine("You have arrived at Iacon. You collect resources here and prepare to fight.");
+    WriteLine("You find Optimus, would you like to inform him of the threat from the Decepticons? (Y or N)");
+    switch (GetChoice())
+    {
+        case Y:
+            WriteLine("Optimus thanks you and you the Autobots and progress on your journey to fight the Decepticons!");
+            currentRegion = Altihex;
+            break;
+        case N:
+            WriteLine("You changed your mind and decided to continue alone, good luck!");
+            currentRegion = Altihex2;
+            break;
+    }
+}
+void AltihexLoop()
+{
+    WriteLine("You have arrived at Altihex.");
+    WriteLine(""); // I'm guessing some type of fight will happen
+
+
+    WriteLine("Are you ready to continue your journey to fight the Decepticons? (Y)es or (N)o? ");
+    switch (GetChoice())
+    {
+        case Y:
+            WriteLine("Great!");
+            currentRegion = Tyrest;
+            break;
+        case N:
+            WriteLine("That's too bad, maybe we'll see you another time!");
+            currentRegion = LithiumFlats;
+            break;
+
+    }
+}
+void TyrestLoop()
+{
+    WriteLine("You have arrived at Tyrest.");
+    WriteLine(""); // I'm guessing some type of fight will happen
+
+
+    WriteLine("Are you ready to continue your journey to fight the Decepticons? (Y)es or (N)o? ");
+    switch (GetChoice())
+    {
+        case Y:
+            WriteLine("Great!");
+            currentRegion = Stanix;
+            break;
+        case N:
+            WriteLine("That's too bad, maybe we'll see you another time!");
+            currentRegion = LithiumFlats;
+            break;
+
+    }
+}
+void StanixLoop()
+{
+    WriteLine("You have arrived at Stanix.");
+    WriteLine(""); // I'm guessing some type of fight will happen
+
+
+    WriteLine("Are you ready to continue your journey to fight the Decepticons? (Y)es or (N)o? ");
+    switch (GetChoice())
+    {
+        case Y:
+            WriteLine("Great!");
+            currentRegion = AcidWastes;
+            break;
+        case N:
+            WriteLine("That's too bad, maybe we'll see you another time!");
+            currentRegion = LithiumFlats;
+            break;
+
+    }
 }
 
 
+//Destination progression if chose x
+void Praxus2Loop()
+{
+    WriteLine("You have arrived at Praxus.");
+    WriteLine(""); // I'm guessing some type of fight will happen
+
+
+    WriteLine("Are you ready to continue your journey to fight the Decepticons? (Y)es or (N)o? ");
+    switch (GetChoice())
+    {
+        case Y:
+            WriteLine("Great!");
+            currentRegion = Iacon2;
+            break;
+        case N:
+            WriteLine("That's too bad, maybe we'll see you another time!");
+            currentRegion = LithiumFlats;
+            break;
+
+    }
+}
+void Iacon2Loop()
+{
+    WriteLine("You have arrived at Iacon.");
+    WriteLine("You collect resources here and prepare for battle."); // Idk what specific stuff but you can come up with that bc you know transformers better than me
+    WriteLine("Are you ready to continue your journey to fight the Decepticons? (Y)es or (N)o? ");
+    switch (GetChoice())
+    {
+        case Y:
+            WriteLine("Great!");
+            currentRegion = Altihex2;
+            break;
+        case N:
+            WriteLine("That's too bad, maybe we'll see you another time!");
+            currentRegion = LithiumFlats;
+            break;
+
+    }
+}
+void Altihex2Loop()
+{
+    WriteLine("You have arrived at Altihex.");
+    WriteLine(""); // I'm guessing some type of fight will happen
+
+
+    WriteLine("Are you ready to continue your journey to fight the Decepticons? (Y)es or (N)o? ");
+    switch (GetChoice())
+    {
+        case Y:
+            WriteLine("Great!");
+            currentRegion = Tyrest2;
+            break;
+        case N:
+            WriteLine("That's too bad, maybe we'll see you another time!");
+            currentRegion = LithiumFlats;
+            break;
+
+    }
+}
+void Tyrest2Loop()
+{
+    WriteLine("You have arrived at Tyrest. It feels lonely by yourself, but you're determined to see this journey to the end.");
+    WriteLine(""); // I'm guessing some type of fight will happen
+
+
+    WriteLine("Are you ready to continue your journey to fight the Decepticons? (Y)es or (N)o? ");
+    switch (GetChoice())
+    {
+        case Y:
+            WriteLine("Great!");
+            currentRegion = Stanix2;
+            break;
+        case N:
+            WriteLine("That's too bad, maybe we'll see you another time!");
+            currentRegion = LithiumFlats;
+            break;
+
+    }
+}
+void Stanix2Loop()
+{
+    WriteLine("You have arrived at Stanix.");
+    WriteLine(""); // I'm guessing some type of fight will happen
+
+    WriteLine("As you near the Decepticon base you worry you may have made the wrong decision going by yourself...");
+    WriteLine("Are you ready to continue your journey to fight the Decepticons? (Y)es or (N)o? ");
+    switch (GetChoice())
+    {
+        case Y:
+            WriteLine("Great!");
+            currentRegion = AcidWastes2;
+            break;
+        case N:
+            WriteLine("That's too bad, maybe we'll see you another time!");
+            currentRegion = LithiumFlats;
+            break;
+
+    }
+}
+
+//Destination progression for choice c
+void NyonLoop()
+{ 
+    WriteLine("You have arrived at Nyon.");
+    WriteLine(""); // I'm guessing some type of fight will happen
+
+
+    WriteLine("Are you ready to continue your journey to help the Decepticons beat the Autobots? (Y or N)");
+    switch (GetChoice())
+    {
+        case Y:
+            WriteLine("Great!");
+            currentRegion = Rodion;
+            break;
+        case N:
+            WriteLine("That's too bad, maybe we'll see you another time!");
+            currentRegion = LithiumFlats;
+            break;
+
+    }
+}
+void RodionLoop()
+{ 
+    WriteLine("You have arrived at Rodion.");
+    WriteLine(""); // I'm guessing some type of fight will happen
+
+
+    WriteLine("Are you ready to continue your journey to help the Decepticons beat the Autobots? (Y or N)");
+    switch (GetChoice())
+    {
+        case Y:
+            WriteLine("Great!");
+            currentRegion = HydraxCity;
+            break;
+        case N:
+            WriteLine("That's too bad, maybe we'll see you another time!");
+            currentRegion = LithiumFlats;
+            break;
+
+    }
+}
+void HydraxCityLoop()
+{
+    WriteLine("You have arrived at Hydrax City.");
+    WriteLine("As you grow nearer to Acid Wastes you start to wonder if conquering the universe with Megatron is your true desire.");
+    WriteLine(""); // I'm guessing some type of fight will happen
+
+
+    WriteLine("Are you ready to continue your journey to help the Decepticons beat the Autobots? (Y or N)");
+    switch (GetChoice())
+    {
+        case Y:
+            WriteLine("Great!");
+            currentRegion = Stanix3;
+            break;
+        case N:
+            WriteLine("That's too bad, maybe we'll see you another time!");
+            currentRegion = LithiumFlats;
+            break;
+
+    }
+}
+void Stanix3Loop()
+{
+    WriteLine("You have arrived at Stanix.");
+    WriteLine("You miss home. Will life ever be the same after betraying your family and city? Is conquering the universe worth losing your soul?");
+    WriteLine(""); // I'm guessing some type of fight will happen
+
+
+    WriteLine("Are you ready to continue your journey to help the Decepticons beat the Autobots? (Y or N)");
+    switch (GetChoice())
+    {
+        case Y:
+            WriteLine("Great!");
+            currentRegion = AcidWastes3;
+            break;
+        case N:
+            WriteLine("That's too bad, maybe we'll see you another time!");
+            currentRegion = LithiumFlats;
+            break;
+
+    }
+}
+
+
+
+//endings
+void AcidWastesLoop()
+{
+    WriteLine("You have arrived at the Acid Wastes, get ready to fight Decepticon!");
+    WriteLine("You approach the base with *insert whoever* and stand ready to fight");
+    //you can write out the fight story line because I don't really know what everyone can do etc.
+    WriteLine("The Autobots won! Megatron was no competition for the mighty Optimus Prime and the Autobots. The universe is saved and Cybertron remains safe!");
+    WriteLine("You Won!");
+    currentRegion = null;
+}
+
+void AcidWastes2Loop()
+{
+    WriteLine("You have arrived at the Acid Wastes, get ready to fight Decepticon!");
+    WriteLine("As you gaze at the base, a feeling of impending doom and fear lingers upon you.");
+    WriteLine("Do you have the power necessary to defeat Megatron by yourself? If you fail what will happen to the world? Should you turn back?");
+    WriteLine("It's too late to turn back now, you must defeat Megatron and the Decepticons.");
+    //once again you can finish and adjust dialogue
+    WriteLine("You were too weak, the overwhelming power of Megatron was too much for you, a singular weak autobot, to overcome. Hopefully the Autobots hear of Megatron's plan and are successful unlike you.");
+    WriteLine("Game Over.")
+    currentRegion = null;
+}
+
+void AcidWastes3Loop()
+{
+    WriteLine("You have arrived at the Acid Wastes, be ready to meet the Decepticons!");
+    WriteLine("As you approach the base you start to feel conviction in your heart for the choice you have made.");
+    WriteLine("Do you really think the Decepticons are capable of defeating someone like Optimus Prime?");
+    WriteLine("With you there to help them of course they do!....Right?");
+    WriteLine("As you're being introduced to Megatron and the rest of the Decepticons you see the autobots approaching, get ready to fight them.");
+    WriteLine(""); //idk stuff about the fight
+    WriteLine("Everything has fallen apart, nothing is going to plan, you lie there injured and quickly dying. Why did you ever choose to side with Megatron? Why did you want the power?");
+    WriteLine("Game Over.");
+    currentRegion = null;
+}
