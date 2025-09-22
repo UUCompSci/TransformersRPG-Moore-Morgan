89fddafadf565d72d8b1c4a9412317b7909fa741
Jacob Moore <104210468+Moore-Jacob@users.noreply.github.com>
2025-09-22 11:21:34 -0500

    Finished Character Selection

--- TransformersTextRPG/Program.cs
+++ TransformersTextRPG/Program.cs
@@ -1,4 +1,5 @@
-﻿//Bytes for character Selection
+﻿using static System.Console;
+//Bytes for character Selection
 const byte BumbleBee = 0;
 const byte SmokeScreen = 1;
 const byte Warpath = 2;
@@ -10,10 +11,12 @@ const ConsoleKey X = ConsoleKey.X;
 const ConsoleKey C = ConsoleKey.C;
 const ConsoleKey V = ConsoleKey.V;
 const ConsoleKey ESC = ConsoleKey.Escape;
-
-
-//For Character Selection
-int CharacterNumber;
+ConsoleKey CharacterChoice()
+{
+    ConsoleKey choice = ReadKey(true).Key;
+    Console.WriteLine();
+    return choice;
+}
 
 //Three main stats that class determines the baseline
 int Speed;
@@ -28,42 +31,77 @@ string[] Weapon;
 //Starts the character selection 
 if (args.Length == 0)
 {
+    WriteLine();
     Console.WriteLine("Default Character: 'BumbleBee' Selected");
     Speed = 80;
     Strength = 50;
     Health = 40;
     MaxHealth = 40;
     AltMode = new string[] { "Small Car" };
-    Weapon = new string[] { "Repeater Blaster" };
+    Weapon = new string[] { "Subsonic Repeater" };
 }
 else
 {
+    WriteLine();
     Console.WriteLine("Characters Available:");
-    Console.WriteLine("BumbleBee(1): Speed = 80, Strength = 30, Health = 40");
-    Console.WriteLine("SmokeScreen(2): Speed = 50, Strength = 50, Health = 50");
-    Console.WriteLine("Warpath: Speed(3) = 30, Strength = 70, Health = 50");
-    bool CharacterSelectionState = true;
+    Console.WriteLine("BumbleBee(Z): Speed = 80, Strength = 30, Health = 40");
+    Console.WriteLine("SmokeScreen(X): Speed = 50, Strength = 50, Health = 50");
+    Console.WriteLine("Warpath: Speed(C) = 30, Strength = 70, Health = 50");
+    bool CharacterSelectionState = false;
 
-    while (CharacterSelectionState)
+    while (!CharacterSelectionState)
     {
-        Console.WriteLine();
-        switch (SelectedCharacter)
+        switch (CharacterChoice())
         {
-            case BumbleBee:
+            case Z:
                 Console.WriteLine("Character Selected: BumbleBee");
+                SelectedCharacter = BumbleBee;
+                Speed = 80;
+                Strength = 50;
+                Health = 40;
+                MaxHealth = 40;
+                AltMode = new string[] { "Small Car" };
+                Weapon = new string[] { " Subsonic Repeater" };
+                CharacterSelectionState = true;
+            
                 break;
+            
 
 
-            case SmokeScreen:
+            case X:
                 Console.WriteLine("Character Selected: SmokeScreen");
+                SelectedCharacter = SmokeScreen;
+                Speed = 50;
+                Strength = 50;
+                Health = 50;
+                MaxHealth = 50;
+                AltMode = new string[] { "Car" };
+                Weapon = new string[] { "Scatter Blaster" };
+                CharacterSelectionState = true;
                 break;
 
-            case Warpath:
+            case C:
                 Console.WriteLine("Character Selected: Warpath");
+                SelectedCharacter = Warpath;
+                Speed = 30;
+                Strength = 70;
+                Health = 50;
+                MaxHealth = 50;
+                AltMode = new string[] { "Tank" };
+                 Weapon = new string[] { "Riot Cannon" };
+                CharacterSelectionState = true;
                 break;
 
             default:
-                Console.WriteLine("Selected Deafault Character: BumbleBee");
+                Console.WriteLine("Deafault Character Selected: BumbleBee");
+                SelectedCharacter = BumbleBee;
+                Speed = 80;
+                Strength = 50;
+                Health = 40;
+                MaxHealth = 40;
+                AltMode = new string[] { "Small Car" };
+                Weapon = new string[] { "Subsonic Repeater" };
+                CharacterSelectionState = true;
             break;
         }
     }
